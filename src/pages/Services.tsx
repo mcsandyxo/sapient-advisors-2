70a5596214fa3b278999754047faa9c2e5686835
cedefrespo <pupi.buzz@gmail.com>
2025-07-08 19:40:59 -0300

    Remove Industries section completely from Services page
    - Removed Industries component import and usage
    - Industries section was not present in the design screenshots
    - Services page now only contains the sections that match the design:
      * Expertise & Services
      * Client Success
      * Success Stories
    - Page now accurately reflects the provided design screenshots

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,5 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Industries from '../components/Industries';
 import ExpertiseServices from '../components/ExpertiseServices';
 import ClientSuccess from '../components/ClientSuccess';
 import SuccessStories from '../components/SuccessStories';
@@ -10,9 +9,6 @@ const Services = () => {
     <div className="min-h-screen bg-white">
       <Header />
       
-      {/* Industries Section */}
-      <Industries />
-
       {/* Expertise & Services Section */}
       <ExpertiseServices />
 
